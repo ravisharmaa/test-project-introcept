a5f9e9e56c498fb5b145246bfff52707f2214f11
ravisharmaa <bastola.ravi@gmail.com>
2017-09-12 14:17:00 +0545

    merge

--- contact-api/app/Http/Middleware/CorsMiddleware.php
+++ contact-api/app/Http/Middleware/CorsMiddleware.php
@@ -24,6 +24,7 @@ class CorsMiddleware
 			->header('Access-Control-Allow-Origin',env('CLIENT_URL'))
 			->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 			->header('Access-Control-Max-Age', '1000')
-			->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+			->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+	        ->header('Access-Control-Allow-Credentials: true');
 	}
 }
